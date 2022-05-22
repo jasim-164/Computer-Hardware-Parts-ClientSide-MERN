2f0020cd926b57d29fa24cb85897a406cd3a7cc4
jasim-164 <jasimuddincse1703164@gmail.com>
2022-05-23 00:04:40 +0600

    banner added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 // import '@fortawesome/fontawesome-free/css/all.min.css';
-// import 'bootstrap-css-only/css/bootstrap.min.css';
+//import 'bootstrap-css-only/css/bootstrap.min.css';
 // import 'mdbreact/dist/css/mdb.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,40 +1,43 @@
-import React from 'react';
-
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import photo1 from "../../assets/photos/carousel/build-your-pc.jpg"
+import photo2 from "../../assets/photos/carousel/free-home-delivery-982x500.jpg"
+import photo3 from "../../assets/photos/carousel/computer-vector.png"
 const Banner = () => {
-    return (
-        <div>
-        <div class="carousel w-full">
-        <div id="slide1" class="carousel-item relative w-full">
-          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2" class="w-full"/> / 
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" class="btn btn-circle">❮</a> 
-            <a href="#slide2" class="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide2" class="carousel-item relative w-full">
-          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=500B67FB" class="w-full"/> / 
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" class="btn btn-circle">❮</a> 
-            <a href="#slide3" class="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide3" class="carousel-item relative w-full">
-          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=A89D0DE6" class="w-full"/> / 
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" class="btn btn-circle">❮</a> 
-            <a href="#slide4" class="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide4" class="carousel-item relative w-full">
-          <img src="https://api.lorem.space/image/car?w=800&h=200&hash=225E6693" class="w-full"/> / 
-          <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" class="btn btn-circle">❮</a> 
-            <a href="#slide1" class="btn btn-circle">❯</a>
-          </div>
-        </div>
+  return (
+  
+    <div class="carousel w-full">
+    <div id="slide1" class="carousel-item relative w-full">
+      <img src={photo1} class="w-full"/> 
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide4" class="btn btn-circle">❮</a> 
+        <a href="#slide2" class="btn btn-circle">❯</a>
       </div>
-        </div>
-    );
+    </div> 
+    <div id="slide2" class="carousel-item relative w-full">
+      <img src={photo2} class="w-full"/> 
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide1" class="btn btn-circle">❮</a> 
+        <a href="#slide3" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide3" class="carousel-item relative w-full">
+      <img src={photo3} class="w-full"/> 
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide2" class="btn btn-circle">❮</a> 
+        <a href="#slide4" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide4" class="carousel-item relative w-full">
+      <img src={photo3} class="w-full h-auto"/> 
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide3" class="btn btn-circle">❮</a> 
+        <a href="#slide1" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+  </div>
+   
+  );
 };
 
 export default Banner;
