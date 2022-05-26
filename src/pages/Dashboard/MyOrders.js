696aa01500dfcddbfd1e86094b55c13a4b9391fc
jasim-164 <jasimuddincse1703164@gmail.com>
2022-05-26 18:40:12 +0600

    my orders page and booking items to DB added

--- src/pages/Dashboard/MyOrders.js
+++ src/pages/Dashboard/MyOrders.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { Link, useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
 
 const MyOrders = () => {
+    const [bookings, setBooking] = useState([]);
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (user) {
+            fetch(` http://localhost:8000/booking?email=${user?.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+
+                    setBooking(data);
+                });
+        }
+    }, [user]);
+
     return (
         <div>
-            <h1 className="text-center pt-10">MyOrders</h1>
-            
+        <h2>My Appointments: {bookings.length}</h2>
+        <div class="overflow-x-auto">
+            <table class="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Customer Name</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>User Email</th>
+                        <th>Payment</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        bookings.map((value, index) => <tr key={value._id}>
+                            <th>{index + 1}</th>
+                            <td>{value?.customerName}</td>
+                            <td>{value?.productName}</td>
+                            <td>{value?.quantity}</td>
+                            <td>{value?.email}</td>
+                            {(!value?.price && !value.paid) && <Link to={`/dashboard/payment/${value._id}`}><button className='btn btn-xs btn-success'>pay</button></Link>}
+                            <td> {(value.price && value.paid) && <div>
+                                <p><span className='text-success'>Paid</span></p>
+                                <p>Transaction id: <span className='text-success'>{value.transactionId}</span></p>
+                            </div>}</td>
+                      
+                        </tr>)
+                    }
+
+
+                </tbody>
+            </table>
         </div>
+    </div>
     );
 };
 
--- src/pages/HardwareComponents/OrderProduct.js
+++ src/pages/HardwareComponents/OrderProduct.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import auth from "../../firebase.init";
 import Loading from "../Shared/Loading";
 
-const OrderProduct = ({product}) => {
+const OrderProduct = () => {
 const {id}= useParams();
 let [value,setValue]= useState(0);
 let [userPrice,setPrice]=useState();
-
+const [user, loading, error] = useAuthState(auth);
+const {
+  register,
+  formState: { errors },
+  reset,
+  handleSubmit,
+} = useForm();
 
 // const {data}= useQuery(["product","_id"],fetch(`http://localhost:8000/products/${id}`).then(result => result.json()));
 // console.log(data);
 // console.log(id);
 const fetchUsers = async () => {
+ 
     
     const res = await fetch(`http://localhost:8000/products/${id}`);
     return res.json();
   };
-  const{data,isLoading,refetch}= useQuery('product',fetchUsers)
+  const{data:product,isLoading,refetch}= useQuery('product',fetchUsers)
   if(isLoading) {
    
     return <Loading/>
@@ -26,47 +37,33 @@ const fetchUsers = async () => {
   
 
 
-  const handleQuantityPlus=()=>{
-     
-      value++;
-     
-      if(value<=data?.quantity)
-      {
-        setValue(value);
-        console.log(value);
-        
-      }
-      else{
 
-          value--;
-          
+    
+    const onSubmit = (data1 , event)=>{
+      event.preventDefault();
+      console.log(data1);
+      fetch('http://localhost:8000/booking',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data1)
+      })
+      .then(res=> res.json())
+      .then(result=> {
+        // console.log("result paisi', result);
+        if(result.result.insertedId){
+          toast.success('order successfully added');
+          refetch();
+          reset();
+        }
+        else{
+          toast.error("Failed to add the Product");
+        }
+      })
 
-      }
-      userPrice=Number(value)*Number(data?.price);
-      setPrice(userPrice);
-  }
+    }
 
+    
 
-  
-  const handleQuantityNeg=()=>{
-      value=value-1;
-      console.log(value);
-      if(value>=1)
-      {
-        setValue(value);
-        console.log(value);
-        
-      }
-      else{
-          value++;
-
-      }
-      userPrice=value*data?.price;
-      setPrice(userPrice);
-      
-  }
-  
-  console.log(data);
 
   return (
     <div>
@@ -74,28 +71,25 @@ const fetchUsers = async () => {
       <div class="hero min-h-screen bg-base-200">
         <div class="hero-content flex-col lg:flex-row">
           <img
-            src={data?.picture}
+            src={product?.picture}
             alt="Product"
           />
           
           <div>
-            <h1 class="text-5xl font-bold">Product Name:{data?.name}</h1>
-            <h1 class="text-2xl font-bold">Product Id: {data?._id}</h1>
-            <h1 class="text-5xl font-bold">Price: {data?.price}</h1>
-            <h1 class="text-5xl font-bold">Available Quantity: {data?.quantity}</h1>
+            <h1 class="text-5xl font-bold mb-3">Product Name: {product?.name}</h1>
+            <h1 class="text-2xl font-bold mb-3">Product Id: {product?._id}</h1>
+            <h1 class="text-5xl font-bold mb-3">Price: {product?.price}</h1>
+            <h1 class="text-5xl font-bold mb-3">Available Quantity: {product?.quantity}</h1>
           
             <p class="py-6">
-            {data?.description}
+            {product?.description}
             </p>
 
             
-            <div className=" flex justify-center align-center lg:flex-row">
-            <button class="btn btn-primary mx-5" onClick={()=>handleQuantityPlus()}>+</button>
-            <h1>{value}</h1>
-            <button class="btn btn-primary" onClick={()=>handleQuantityNeg()}>-</button>
-            
-            </div>
-            <button class="btn  btn-ghost " onClick={()=>handleQuantityNeg()} disabled={value===0}>Order Now</button>
+
+
+
+
         
             
            
@@ -105,6 +99,78 @@ const fetchUsers = async () => {
           </div>
         </div>
       </div>
+      
+      <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="text-center text-2xl font-bold">Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              readOnly value={user?.email || ''}
+              className="input input-bordered w-full max-w-xs"
+              {...register("email")}
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+             
+              readOnly value={user?.displayName || ''}
+              className="input input-bordered w-full max-w-xs"
+              {...register("customerName")}
+            />
+    
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Quantity</span>
+            </label>
+            <input
+              type="number"
+              placeholder="Please place a valid Quantity that you want to Buy"
+              
+              className="input input-bordered w-full max-w-xs"
+              {...register("quantity", {required: true, message: "Please place a valid Quantity",min: 1, max: product?.quantity })}
+            />
+            <label className="label">
+              {errors.quantity?.type === "required" && (
+                <span className="label-text-alt text-red-500">
+                  <h1>please enter a Quantity</h1>
+                </span>
+              )}
+              {errors.quantity?.type==="min" && (
+                <span className="label-text-alt text-red-500">
+                  <h1>please enter a minimum valid Quantity</h1>
+                </span>
+              )}
+              {errors.quantity?.type==="max" && (
+                <span className="label-text-alt text-red-500">
+                  <h1>please enter a maxmimum valid Quantity</h1>
+                </span>
+              )}
+            </label>
+          </div>
+
+         
+          <input
+            className="btn w-full max-w-xs text-white"
+            type="submit"
+            value="Order"
+          />
+        </form>
+  
+      </div>
+    </div>
+
+
+
     </div>
   );
 };
